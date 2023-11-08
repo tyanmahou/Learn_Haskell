2fab927f877a9997940303f8f4761427e79754eb
tyanmahou <tyanmahou@gmail.com>
2023-11-09 00:50:49 +0900

    done chapter1

--- src/chapter1/baby.hs
+++ src/chapter1/baby.hs
@@ -7,3 +7,16 @@ doubleSmallNumber x =  if x > 100
                         else x * 2
 doubleSmallNumber' x = doubleSmallNumber x + 1
 
+boomBangs :: Integral a => [a] -> [String]
+boomBangs xs = [if x < 10 then "BOOM!" else "BANG!"|x<-xs, odd x]
+
+fizzbuzz xs = [
+    if (x `mod` 3) == 0 && (x `mod` 5) == 0 then "FizzBuzz" 
+    else if x `mod` 3 == 0 then "Fizz"
+    else if x `mod` 5 == 0 then "Buzz"
+    else show x | x <-xs
+    ]
+
+length' xs = sum [1 | _ <-xs]    
+
+removeNonUpperCase st = [c | c<-st, c `elem` ['A'..'Z']]

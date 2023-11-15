842626d76ce7e16d63dea97d122f5d3404ce9ef2
tyanmahou <tyanmahou@gmail.com>
2023-11-16 00:18:03 +0900

    update Dockerfile

--- src/chapter4/baby.hs
+++ src/chapter4/baby.hs
@@ -1,39 +1,39 @@
 maximum' :: (Ord a) => [a] -> a
 maximum' [] = error "maximum of empty list"
 maximum' [x] = x
-maximum' (x:xs) = max x (maximum' xs)
+maximum' (x : xs) = max x (maximum' xs)
 
-replicate' :: Int ->a->[a]
+replicate' :: Int -> a -> [a]
 replicate' n x
- | n <= 0 = []
- | otherwise = x : replicate' (n - 1) x 
+  | n <= 0 = []
+  | otherwise = x : replicate' (n - 1) x
 
-take' ::Int ->[a]->[a]
+take' :: Int -> [a] -> [a]
 take' n _
- | n <= 0 = []
+  | n <= 0 = []
 take' _ [] = []
-take' n (x:xs) = x : take' (n-1) xs   
+take' n (x : xs) = x : take' (n - 1) xs
 
-drop' ::Int ->[a]->[a]
+drop' :: Int -> [a] -> [a]
 drop' n xs
- | n <= 0 = xs
+  | n <= 0 = xs
 drop' _ [] = []
-drop' n (x:xs) = drop' (n-1) xs
+drop' n (x : xs) = drop' (n - 1) xs
 
-reverse' :: [a]->[a]
+reverse' :: [a] -> [a]
 reverse' [] = []
-reverse' (x:xs) = reverse' xs ++ [x]
+reverse' (x : xs) = reverse' xs ++ [x]
 
-repeat' :: a->[a]
-repeat' x = x: repeat' x
+repeat' :: a -> [a]
+repeat' x = x : repeat' x
 
-zip' :: [a]->[b]->[(a, b)]
+zip' :: [a] -> [b] -> [(a, b)]
 zip' [] _ = []
 zip' _ [] = []
-zip' (x:xs) (y:ys) = (x, y):zip' xs ys
+zip' (x : xs) (y : ys) = (x, y) : zip' xs ys
 
-elem'::(Eq a) => a -> [a] -> Bool
+elem' :: (Eq a) => a -> [a] -> Bool
 elem' a [] = False
-elem' a (x:xs)
- | a == x = True
- | otherwise = elem' a xs
+elem' a (x : xs)
+  | a == x = True
+  | otherwise = elem' a xs

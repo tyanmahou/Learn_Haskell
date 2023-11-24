8ebc23e3ed4666bcd82f7b107c9ee64b9172ffab
tyanmahou <tyanmahou@gmail.com>
2023-11-24 16:25:43 +0900

    update chapter7

--- src/chapter7/baby.hs
+++ src/chapter7/baby.hs
@@ -70,4 +70,10 @@ lockers = Map.fromList [
   (101, (Free, "JAH3I"))
  ]                                           
 
-data List a = Empty | Cons {listhead :: a, listtail :: List a} deriving (Show, Read, Eq, Ord)
+infixr 5 :-:
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+infixr 5 ^++
+(^++) :: List a -> List a -> List a
+Empty ^++ ys = ys
+(x :-: xs) ^++ ys = x :-: (xs ^++ ys)

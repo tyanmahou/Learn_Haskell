47e054669f8e6ef9dd606380137352ceab216b95
tyanmahou <tyanmahou@gmail.com>
2023-11-26 22:52:58 +0900

    done chapter7

--- src/chapter7/baby.hs
+++ src/chapter7/baby.hs
@@ -79,3 +79,68 @@ Empty ^++ ys = ys
 (x :-: xs) ^++ ys = x :-: (xs ^++ ys)
 
 data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving(Show)
+instance Functor Tree where
+  fmap f EmptyTree = EmptyTree
+  fmap f (Node x left right) = Node (f x) (fmap f left) (fmap f right)
+  
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+ | x == a = Node x left right
+ | x < a = Node a (treeInsert x left) right
+ | x > a = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a ->  Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+ | x == a = True
+ | x < a = treeElem x left
+ | x > a = treeElem x right
+
+data TrafficLight = Red | Yellow | Green
+instance Eq TrafficLight where
+  (==) :: TrafficLight -> TrafficLight -> Bool
+  Red == Red = True
+  Yellow == Yellow = True
+  Green == Green = True
+  _ == _ = False
+
+instance Show TrafficLight where
+  show :: TrafficLight -> String
+  show Red = "Red light"
+  show Yellow = "Yellow light"    
+  show Green = "Green light"
+
+class YesNo a where
+  yesno :: a -> Bool
+
+instance YesNo Int where
+  yesno 0 = False
+  yesno _ = True
+
+instance YesNo [a] where
+  yesno [] = False
+  yesno _ = True
+
+instance YesNo Bool where
+  yesno = id
+
+instance YesNo (Maybe a) where
+  yesno (Just _) = True
+  yesno Nothing = False
+
+instance YesNo (Tree a) where
+  yesno EmptyTree = True
+  yesno _ = False  
+
+instance YesNo TrafficLight where
+  yesno Red = False
+  yesno _ = True  
+
+yesnoIf :: (YesNo y) => y -> a -> a -> a
+yesnoIf yesnoVal yesResult noResult = 
+  if yesno yesnoVal then yesResult
+                    else noResult

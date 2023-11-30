f8f029e568c3408fc1a1dfb282dce21e680d328e
tyanmahou <tyanmahou@gmail.com>
2023-11-30 23:42:18 +0900

    update chapter9

---
+++ src/chapter9/palindromes.hs
@@ -0,0 +1,13 @@
+import Control.Monad
+import Data.Char
+
+main = interact respondPalindromes
+
+respondPalindromes :: String -> String
+respondPalindromes =
+    unlines .
+    map (\xs -> if isPal xs then "palindrome" else "not a palindrome") .
+    lines
+
+isPal :: String -> Bool
+isPal xs = xs == reverse xs

15bf2019b2b040681aff225309f22cdfba420f44
tyanmahou <tyanmahou@gmail.com>
2023-12-11 23:21:24 +0900

    update chapter10

---
+++ src/chapter10/baby.hs
@@ -0,0 +1,10 @@
+solveRPN :: String -> Double
+solveRPN = head . foldl foldingFunction [] . words
+  where foldingFunction (x:y:ys) "*" = (y * x):ys
+        foldingFunction (x:y:ys) "+" = (y + x):ys
+        foldingFunction (x:y:ys) "-" = (y - x):ys        
+        foldingFunction (x:y:ys) "/" = (y / x):ys        
+        foldingFunction (x:y:ys) "^" = (y ** x):ys        
+        foldingFunction (x:ys) "ln" = log x:ys        
+        foldingFunction xs "sum" = [sum xs]      
+        foldingFunction xs numberString = read numberString:xs                

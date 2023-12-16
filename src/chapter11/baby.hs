e374ae32017d80c1183dee2d30602f07af4afb55
tyanmahou <tyanmahou@gmail.com>
2023-12-16 23:35:27 +0900

    update chapter11

--- src/chapter11/baby.hs
+++ src/chapter11/baby.hs
@@ -1,6 +1,8 @@
+import Control.Applicative (Applicative(liftA2))
 myAction :: IO String
 myAction = (++) <$> getLine <*> getLine
 
 sequenceA' :: (Applicative f) => [f a] -> f [a]
-sequenceA' [] = pure []
-sequenceA' (x:xs) = (:) <$> x <*> sequenceA' xs
+-- sequenceA' [] = pure []
+-- sequenceA' (x:xs) = (:) <$> x <*> sequenceA' xs
+sequenceA' = foldr (liftA2 (:)) (pure [])

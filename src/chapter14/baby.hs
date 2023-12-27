c93b2c55313d2538a1aa19cb0319963dd043fe7b
tyanmahou <tyanmahou@gmail.com>
2023-12-27 23:36:52 +0900

    update chapter14

--- src/chapter14/baby.hs
+++ src/chapter14/baby.hs
@@ -25,21 +25,70 @@ multiWithLog = do
   tell ["Gonna multiply these two"]
   return (a * b);
 
-gcd' :: Int -> Int -> Writer [String] Int
-gcd' a b
- | b == 0    = do
-    tell ["Finished with " ++ show a]
-    return a
- | otherwise = do
-    tell  [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
-    gcd' b (a `mod` b)  
+-- gcd' :: Int -> Int -> Writer [String] Int
+-- gcd' a b
+--  | b == 0    = do
+--     tell ["Finished with " ++ show a]
+--     return a
+--  | otherwise = do
+--     tell  [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+--     gcd' b (a `mod` b)  
 
-gcdReverse :: Int -> Int -> Writer [String] Int
-gcdReverse a b
+-- gcdReverse :: Int -> Int -> Writer [String] Int
+-- gcdReverse a b
+--  | b == 0    = do
+--     tell ["Finished with " ++ show a]
+--     return a
+--  | otherwise = do
+--     result <- gcdReverse b (a `mod` b) 
+--     tell  [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+--     return result;
+
+newtype DiffList a = DiffList {getDiffList :: [a] -> [a]}
+
+toDiffList :: [a] -> DiffList a
+toDiffList xs = DiffList (xs ++)
+
+fromDiffList :: DiffList a -> [a]
+fromDiffList (DiffList f) = f []
+
+instance Semigroup (DiffList a) where
+    (DiffList f) <> (DiffList g) = DiffList (f . g)
+instance Monoid (DiffList a) where
+   mempty = DiffList ([] ++)
+
+gcdReverse' :: Int -> Int -> Writer (DiffList String) Int
+gcdReverse' a b
  | b == 0    = do
-    tell ["Finished with " ++ show a]
+    tell $ toDiffList ["Finished with " ++ show a]
     return a
  | otherwise = do
-    result <- gcdReverse b (a `mod` b) 
-    tell  [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
-    return result;
+    result <- gcdReverse' b (a `mod` b) 
+    tell $ toDiffList [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+    return result;    
+
+finalCountDown :: Int -> Writer (DiffList String) ()
+finalCountDown 0 = do
+   tell (toDiffList ["0"])
+finalCountDown x = do
+   finalCountDown (x - 1)
+   tell (toDiffList [show x])
+
+-- finalCountDown :: Int -> Writer [String] ()
+-- finalCountDown 0 = do
+--    tell ["0"]
+-- finalCountDown x = do
+--    finalCountDown (x - 1)
+--    tell [show x]
+
+addStuff :: Int -> Int
+addStuff = do
+   a <- (*2)
+   b <- (+10)
+   return (a + b)
+
+-- addStuff :: Int -> Int
+-- addStuff x = let
+--    a = (*2) x 
+--    b = (+10) x
+--    in (a + b)   

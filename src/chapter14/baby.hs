b8d19fdc42697620fe062aeff23c091d3b144500
tyanmahou <tyanmahou@gmail.com>
2023-12-28 01:19:01 +0900

    update chapter14

--- src/chapter14/baby.hs
+++ src/chapter14/baby.hs
@@ -1,5 +1,6 @@
 import Data.Monoid
 import Control.Monad.Writer
+import Control.Monad.State
 
 isBigGang :: Int -> (Bool, String)
 isBigGang x = (x > 9, "Compared gang size to 9.")
@@ -92,3 +93,68 @@ addStuff = do
 --    a = (*2) x 
 --    b = (+10) x
 --    in (a + b)   
+
+
+type Stack = [Int]
+
+-- pop :: Stack -> (Int, Stack)
+-- pop (x:xs) = (x, xs)
+
+-- push :: Int -> Stack -> ((), Stack)
+-- push a xs  = ((), a:xs)
+
+-- stackManip :: Stack -> (Int, Stack)
+-- stackManip stack = let
+--    ((), newStack1) = push 3 stack
+--    (a, newStack2) = pop newStack1
+--    in pop newStack2
+
+-- pop :: State Stack Int
+-- pop = state $ \(x:xs) -> (x, xs)
+
+-- push :: Int -> State Stack ()
+-- push a = state $ \xs -> ((), a:xs)
+
+stackManip :: State Stack Int
+stackManip = do
+   push 3
+   a <- pop
+   pop
+
+stackStuff :: State Stack ()
+stackStuff = do
+   a <- pop
+   if a == 5
+      then push 5
+      else do
+         push 3
+         push 8
+
+moreStack :: State Stack ()
+moreStack = do
+   a <- stackManip
+   -- if a == 1000
+   --    then stackStuff
+   --    else return ()
+   when (a == 1000) stackStuff
+
+stacklyStack :: State Stack ()   
+stacklyStack = do
+   stackNow <- get
+   if stackNow == [1,2,3]
+      then put [8,3,1]
+      else put [9,2,1]      
+
+pop :: State Stack Int
+pop = do
+   xl <- get
+   case xl of
+      (x:xs) -> do
+            put xs
+            return x
+      _  -> error "Error"
+
+push :: Int -> State Stack ()
+push x = do
+   xs <- get
+   put (x:xs)

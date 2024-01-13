d2d10ed5db47b013063216f8644a05a07331b106
tyanmahou <tyanmahou@gmail.com>
2024-01-13 22:42:28 +0900

    update chapter15

---
+++ src/chapter15/list.hs
@@ -0,0 +1,9 @@
+data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+
+type ListZipper a = ([a], [a])
+
+goForward :: ListZipper a -> ListZipper a
+goForward (x:xs, bs) = (xs, x:bs)
+
+goBack :: ListZipper a -> ListZipper a
+goBack (xs, b:bs) = (b:xs, bs)

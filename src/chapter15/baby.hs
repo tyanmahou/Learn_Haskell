78ddb23f51f973beb99f92f220e9a91c72bc6e65
tyanmahou <tyanmahou@gmail.com>
2024-01-17 23:09:18 +0900

    update chapter15

--- src/chapter15/baby.hs
+++ src/chapter15/baby.hs
@@ -51,15 +51,15 @@ x -: f = f x
 data Crumb a = LeftCrumb a (Tree a) | RightCrumb a (Tree a) deriving(Show)
 type Breadcrumbs a = [Crumb a]
 
-goLeft :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
-goLeft (Node x l r, bs) = (l, LeftCrumb x r:bs)
+-- goLeft :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
+-- goLeft (Node x l r, bs) = (l, LeftCrumb x r:bs)
 
-goRight :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
-goRight (Node x l r, bs) = (r, RightCrumb x l:bs)
+-- goRight :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
+-- goRight (Node x l r, bs) = (r, RightCrumb x l:bs)
 
-goUp :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
-goUp (t, LeftCrumb x r:bs) = (Node x t r, bs)
-goUp (t, RightCrumb x l:bs) = (Node x l t, bs)
+-- goUp :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
+-- goUp (t, LeftCrumb x r:bs) = (Node x t r, bs)
+-- goUp (t, RightCrumb x l:bs) = (Node x l t, bs)
 
 type Zipper a = (Tree a, Breadcrumbs a)
 
@@ -67,6 +67,23 @@ modify :: (a -> a) -> Zipper a -> Zipper a
 modify f (Node x l r, bs) = (Node (f x) l r, bs)
 modify f (Empty, bs) = (Empty, bs)
 
-topMost :: Zipper a -> Zipper a
-topMost (t, []) = (t, [])
-topMost z = topMost (goUp z)
+-- topMost :: Zipper a -> Zipper a
+-- topMost (t, []) = (t, [])
+-- topMost z = topMost (goUp z)
+
+goLeft :: Zipper a -> Maybe (Zipper a)
+goLeft (Node x l r, bs) = Just (l, LeftCrumb x r:bs)
+goLeft (Empty, _) = Nothing
+
+goRight :: Zipper a -> Maybe (Zipper a)
+goRight (Node x l r, bs) = Just (r, RightCrumb x l:bs)
+goRight (Empty, _) = Nothing
+
+goUp :: Zipper a -> Maybe (Zipper a)
+goUp (t, LeftCrumb x r:bs) = Just (Node x t r, bs)
+goUp (t, RightCrumb x l:bs) = Just (Node x l t, bs)
+goUp (_, []) = Nothing
+
+topMost :: Zipper a -> Maybe (Zipper a)
+topMost (t, []) = Just (t, [])
+topMost z = topMost =<< goUp z
